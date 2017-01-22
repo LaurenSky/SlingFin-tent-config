3df1122902e883c07f92f12566ec752648badba1
LaurenSky <ljherrer@gmail.com>
2017-01-22 01:43:59 -0800

    added parens to alternet units

--- src/components/TentDimensions/TentDimensions.js
+++ src/components/TentDimensions/TentDimensions.js
@@ -42,8 +42,8 @@ class TentDimensions extends React.Component {
           <li><h5>2 Person Tent</h5></li>
           <li><h5>2 Side Doors with Dry Entries</h5></li>
           <li><h5>Length: 92.25" long</h5></li>
-          <li><h5>Interior Height: 41.5in</h5></li>
-          <li><h5>Floor Area: 32 ft^2 / 2.97m^2</h5></li>
+          <li><h5>Interior Height: 41.5"</h5></li>
+          <li><h5>Floor Area: 32 ft^2 ( 2.97m^2 )</h5></li>
           <li><Volume data={this.props.data}/></li>
           <li><h5>Vestibules: 2 (11 sq ft + 11 sq ft)</h5></li>
           <li><PackSize data={this.props.data}/></li>
--- src/components/TentDimensions/volume.js
+++ src/components/TentDimensions/volume.js
@@ -15,10 +15,10 @@ class Volume extends React.Component {
 
     return (
       <div>
-        <h5>Total Volume: 108.6 cu ft / 3.08 cu m</h5>
+        <h5>Total Volume: 108.6 cu ft (3.08 cu m)</h5>
         <ul>
-          <li><h6>Body Volume: 58.6 cu ft / 1.66 cu m</h6></li>
-          <li><h6>Vestibule Volume: 50.0 cu ft / 1.42 cu m</h6></li>
+          <li><h6>Body Volume: 58.6 cu ft (1.66 cu m)</h6></li>
+          <li><h6>Vestibule Volume: 50.0 cu ft (1.42 cu m)</h6></li>
         </ul>
       </div>
     );
